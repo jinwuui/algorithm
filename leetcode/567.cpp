edd99c4c9fba5219aeeb5887d9c72f58d07b51d2
jinwuui <jinwuui@gmail.com>
2022-02-12 16:31:06 +0900

    22/02/12 1Upsolving

--- leetcode/567.cpp
+++ leetcode/567.cpp
@@ -28,3 +28,34 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) return false;
+        
+        vector<int> s1cnt(26);
+        vector<int> s2cnt(26);
+        for (int i = 0; i < s1.length(); i++) {
+            s1cnt[s1[i] - 'a']++;
+            s2cnt[s2[i] - 'a']++;
+        }
+        
+        for (int i = 0; i < s2.length() - s1.length() + 1; i++) {
+            bool isOk = true;
+            for (int j = 0; j < 26; j++) {
+                if (s1cnt[j] != s2cnt[j]) {
+                    isOk = false;
+                    break;
+                }
+            }
+            if (isOk) return true;
+            
+            s2cnt[s2[i] - 'a']--;
+            if (i + s1.length() < s2.length())
+                s2cnt[s2[i + s1.length()] - 'a']++;
+        }
+        
+        return false;
+    }
+};
