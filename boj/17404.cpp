5b1d4b992f68e4713c38d973cacc0b7a581c4c39
jinwuui <jinwuui@gmail.com>
2022-03-25 10:17:34 +0900

    22/03/25 1Solve

--- boj/17404.cpp
+++ boj/17404.cpp
@@ -18,34 +18,35 @@ const int dc[4] = {0, 1, 0, -1};
 const int MOD = 1000000007;
 
 struct RGB {
-	int r = 0, g = 0, b = 0;
+	int n1, n2, n3;
 	RGB() {}
-	RGB(int nr, int ng, int nb) {
-		r = nr, g = ng, b = nb;
+	RGB(int nn1, int nn2, int nn3) {
+		n1 = nn1, n2 = nn2, n3 = nn3;
 	}
 };
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	
 	int n;
 	cin >> n;
-	vector<vector<RGB>> dp(3, vector<RGB>(n));
+	vector<vector<RGB>> v(n, vector<RGB>(3));
+	int t1, t2, t3;
+	cin >> t1 >> t2 >> t3;
+	v[0][0] = RGB(t1, 1e9, 1e9);
+	v[0][1] = RGB(1e9, t2, 1e9);
+	v[0][2] = RGB(1e9, 1e9, t3);
 
-	// 재귀 DP로 풀어보기
-	for (int i = 0; i < 3; i++) {
-		int tmp;
-		cin >> tmp;
-		dp[i][0] = RGB(tmp, tmp, tmp);
-	}
 	for (int i = 1; i < n - 1; i++) {
 		int r, g, b;
 		cin >> r >> g >> b;
-		dp[0][i] = RGB(-1, r + dp[0][i - 1].g, r + dp[0][i - 1].b);
-		dp[1][i] = RGB(g + dp[0][i - 1].r, -1, g + dp[0][i - 1].b);
-		dp[2][i] = RGB(b + dp[0][i - 1].r, b + dp[0][i - 1].g, -1);
+
+		v[i][0] = RGB(min(v[i - 1][1].n1, v[i - 1][2].n1) + r, min(v[i - 1][1].n2, v[i - 1][2].n2) + r, min(v[i - 1][1].n3, v[i - 1][2].n3) + r);
+		v[i][1] = RGB(min(v[i - 1][0].n1, v[i - 1][2].n1) + g, min(v[i - 1][0].n2, v[i - 1][2].n2) + g, min(v[i - 1][0].n3, v[i - 1][2].n3) + g);
+		v[i][2] = RGB(min(v[i - 1][0].n1, v[i - 1][1].n1) + b, min(v[i - 1][0].n2, v[i - 1][1].n2) + b, min(v[i - 1][0].n3, v[i - 1][1].n3) + b);
 	}
-	
+	int r, g, b;
+	cin >> r >> g >> b;
+	int ans = r + min({v[n - 2][1].n2, v[n - 2][2].n2, v[n - 2][1].n3, v[n - 2][2].n3});
+	ans = min(ans, g + min({v[n - 2][0].n1, v[n - 2][2].n1, v[n - 2][0].n3, v[n - 2][2].n3}));
+	ans = min(ans, b + min({v[n - 2][0].n1, v[n - 2][1].n1, v[n - 2][0].n2, v[n - 2][1].n2}));
+	cout << ans;
 	return 0;
 }
